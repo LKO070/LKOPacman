a65fccaa82a1a6629ba7450199d6a25b74d6b8af
Loek-Kai <loekota@gmail.com>
2024-08-15 11:20:24 +0200

    Update NodeSpawnLogic.cs
    Screen Filled with dots

--- Assets/NodeSpawnLogic.cs
+++ Assets/NodeSpawnLogic.cs
@@ -4,10 +4,24 @@ using UnityEngine;
 
 public class NodeSpawnLogic : MonoBehaviour
 {
+    int numToSpawn = 28;
+
+    public float currentSpawnOffset;
+    public float spawnOffset = 0.3f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (gameObject.name == "Node")
+        {
+            currentSpawnOffset = spawnOffset;
+            for (int i = 0; i < numToSpawn; i++)
+            {
+                //Cloning for new node
+                GameObject nodeClone = Instantiate(gameObject, new Vector3(transform.position.x, transform.position.y + currentSpawnOffset, 0), Quaternion.identity);
+                currentSpawnOffset += spawnOffset;
+            }
+        }
     }
 
     // Update is called once per frame
